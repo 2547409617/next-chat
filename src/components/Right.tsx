aeeddfe6961ac8a8cf3c4b8135bc4d20a866eb29
abc <abc@example.com>
2024-05-14 23:05:36 +0800

    fix

--- src/components/Right.tsx
+++ src/components/Right.tsx
@@ -51,7 +51,7 @@ const Right = () => {
                     <div
                       className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
                     >
-                      <div>I'm ok what about you?</div>
+                    <div>I am ok what about you?</div>
                     </div>
                   </div>
                 </div>
